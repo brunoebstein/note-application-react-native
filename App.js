c782c67fa4fc2821ed90e017cb9e950d9ed6c188
bruno <bruno.ebstein@la-combe-du-lion-vert.fr>
2018-07-06 11:47:01 +0200

    Upd: run pettier on the code base

--- App.js
+++ App.js
@@ -9,8 +9,10 @@ import { NoteListView } from './src/NoteListView/NoteListView';
 
 type Props = {};
 export default class App extends Component<Props> {
-	render() {
-		const notes = [...Array(1).keys()].map(i => ({ title: `my title ${i + 1}` }))
-		return <NoteListView notes={notes} />
-	}
+  render() {
+    const notes = [...Array(1).keys()].map(i => ({
+      title: `my title ${i + 1}`,
+    }));
+    return <NoteListView notes={notes} />;
+  }
 }
--- src/NoteListView/NoteListView.js
+++ src/NoteListView/NoteListView.js
@@ -1,64 +1,60 @@
-import React from 'react'
-import {
-	StyleSheet,
-	Text,
-	View,
-	Button,
-	ScrollView,
-} from 'react-native';
-import PropTypes from 'prop-types'
-
+import React from 'react';
+import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
+import PropTypes from 'prop-types';
 
 export const NoteListView = ({ notes }) => (
-	<View style={styles.container}>
-		{!notes || notes.length === 0
-			? <Text style={styles.welcome}>Vous n'avez pas de notes</Text>
-			: (
-				<ScrollView style={{ flex: 1 }}>
-					{notes.map(note =>
-						<Text style={styles.noteTitle} key={note.title}>{note.title}</Text>
-					)}
-				</ScrollView>
-			)
-		}
-		<View style={styles.addNoteButton}>
-			<Button
-				onPress={() => { debugger }}
-				title="Create"
-			/>
-		</View>
-	</View>
-)
-
+  <View style={styles.container}>
+    {!notes || notes.length === 0 ? (
+      <Text style={styles.welcome}>Vous n'avez pas de notes</Text>
+    ) : (
+      <ScrollView style={{ flex: 1 }}>
+        {notes.map(note => (
+          <Text style={styles.noteTitle} key={note.title}>
+            {note.title}
+          </Text>
+        ))}
+      </ScrollView>
+    )}
+    <View style={styles.addNoteButton}>
+      <Button
+        onPress={() => {
+          debugger;
+        }}
+        title="Create"
+      />
+    </View>
+  </View>
+);
 
 NoteListView.propTypes = {
-	notes: PropTypes.arrayOf(PropTypes.shape({
-		title: PropTypes.string.isRequired
-	}))
-}
-
+  notes: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+    }),
+  ),
+};
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'flex-start',
-		alignItems: 'stretch',
-		backgroundColor: '#F5FCFF',
-	},
-	welcome: {
-		fontSize: 20,
-		textAlign: 'center',
-		margin: 10,
-	},
-	noteTitle: {
-		fontSize: 26,
-		padding: 20,
-		borderBottomColor: 'darkgrey',
-		borderBottomWidth: 1,
-	},
-	addNoteButton: {
-		position: 'absolute',
-		bottom: 20,
-		right: 20,
-	},
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'stretch',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  noteTitle: {
+    fontSize: 26,
+    padding: 20,
+    borderBottomColor: 'darkgrey',
+    borderBottomWidth: 1,
+  },
+  addNoteButton: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+  },
 });
