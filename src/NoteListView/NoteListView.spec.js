0c61711764a02daab823f0d52c4ee0f3d6d67899
bruno <bruno.ebstein@la-combe-du-lion-vert.fr>
2018-07-06 16:07:13 +0200

    Add: theme color

--- src/NoteListView/NoteListView.spec.js
+++ src/NoteListView/NoteListView.spec.js
@@ -1,30 +1,34 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { ThemeProvider } from 'styled-components';
 
 import { NoteListView } from './NoteListView';
 import { range } from '../helper/range';
 
 describe('NoteListView component', () => {
-  it('should display an empty list when no note', () => {
-    const list = renderer.create(<NoteListView />).toJSON();
+  function matchSnapshot(element) {
+    const list = renderer
+      .create(<ThemeProvider theme={{}}>{element}</ThemeProvider>)
+      .toJSON();
     expect(list).toMatchSnapshot();
+  }
+
+  it('should display an empty list when no note', () => {
+    matchSnapshot(<NoteListView />);
   });
 
   it('should display title note when 1 note', () => {
     const notes = [{ title: 'my title' }];
-    const list = renderer.create(<NoteListView notes={notes} />).toJSON();
-    expect(list).toMatchSnapshot();
+    matchSnapshot(<NoteListView notes={notes} />);
   });
 
   it('should display title note when 2 notes', () => {
     const notes = [{ title: 'my title' }, { title: 'my title 2' }];
-    const list = renderer.create(<NoteListView notes={notes} />).toJSON();
-    expect(list).toMatchSnapshot();
+    matchSnapshot(<NoteListView notes={notes} />);
   });
 
   it('should display title note when many notes', () => {
     const notes = range(20).map(i => ({ title: `my title ${i}` }));
-    const list = renderer.create(<NoteListView notes={notes} />).toJSON();
-    expect(list).toMatchSnapshot();
+    matchSnapshot(<NoteListView notes={notes} />);
   });
 });
