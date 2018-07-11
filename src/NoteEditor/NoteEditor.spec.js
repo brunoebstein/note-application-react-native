25e2ce95a29e215bada6440e9f0ce576cc19b991
bruno <bruno.ebstein@la-combe-du-lion-vert.fr>
2018-07-11 16:01:13 +0200

    Refactor: import React in global

--- src/NoteEditor/NoteEditor.spec.js
+++ src/NoteEditor/NoteEditor.spec.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import { NoteEditor } from './NoteEditor';
 
 describe('a NoteEditor', () => {
--- src/setupTests.js
+++ src/setupTests.js
@@ -4,5 +4,8 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 
+import React from 'react';
+global.React = React;
+
 import { shallow } from 'enzyme';
 global.shallow = shallow;
