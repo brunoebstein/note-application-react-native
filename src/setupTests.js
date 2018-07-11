5426bc143ce053e8f9d23d3d44096e5cb52096df
bruno <bruno.ebstein@la-combe-du-lion-vert.fr>
2018-07-11 17:34:40 +0200

    Refactor: use shallow from react test renderer instead of Enzyme

--- src/setupTests.js
+++ src/setupTests.js
@@ -7,7 +7,13 @@ configure({ adapter: new Adapter() });
 import React from 'react';
 global.React = React;
 
-import { shallow } from 'enzyme';
+import ShallowRenderer from 'react-test-renderer/shallow';
+function shallow(element) {
+  const renderer = new ShallowRenderer();
+  renderer.render(element);
+  const wrapper = renderer.getRenderOutput();
+  return wrapper;
+}
 
 global.matchSnapshot = function matchSnapshot(component) {
   const wrapper = shallow(component);
