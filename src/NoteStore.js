665a4d84f30e84effe0505b451b7466e30f5fb16
bruno <bruno.ebstein@la-combe-du-lion-vert.fr>
2018-07-12 18:16:57 +0200

    Del: Redux DevTools Extension use React Native Debugger app instead

--- src/NoteStore.js
+++ src/NoteStore.js
@@ -4,10 +4,7 @@ import { createStore } from 'redux';
 
 import { appReducer } from './appReducer';
 
-const store = createStore(
-  appReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
+const store = createStore(appReducer);
 
 export const NoteStore = ({ children }) => (
   <StoreProvider store={store}>{children}</StoreProvider>
