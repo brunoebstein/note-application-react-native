cc33c01e945c4ff69c1f1719f8e2419ed64c9659
bruno <bruno.ebstein@la-combe-du-lion-vert.fr>
2018-07-12 15:56:58 +0200

    Add: test that check if save action is well dispatched

--- src/NoteEditor/NoteEditorWired.spec.js
+++ src/NoteEditor/NoteEditorWired.spec.js
@@ -2,35 +2,71 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
 import { matchSnapshot } from '../helper/matchSnapshot';
+import { mount } from '../helper/mount';
 
 import { NoteEditorWired } from './NoteEditorWired';
 
 describe('NoteEditorWired component', () => {
-  function matchSnapshotWithProvider(state) {
-    const store = createStore(() => state);
+  describe('simple render', () => {
+    function matchSnapshotWithProvider(state) {
+      const store = createStore(() => state);
 
-    matchSnapshot(
-      <Provider store={store}>
-        <NoteEditorWired />
-      </Provider>,
-    );
-  }
+      matchSnapshot(
+        <Provider store={store}>
+          <NoteEditorWired />
+        </Provider>,
+      );
+    }
 
-  it('should render an empty form without selected note', () => {
-    const state = {
-      notes: [{ title: 'a title', content: 'a content' }],
-      selectedNote: null,
-    };
+    it('should render an empty form without selected note', () => {
+      const state = {
+        notes: [{ title: 'a title', content: 'a content' }],
+        selectedNote: null,
+      };
 
-    matchSnapshotWithProvider(state);
+      matchSnapshotWithProvider(state);
+    });
+
+    it('should render a prefilled form with selected note', () => {
+      const state = {
+        notes: [{ title: 'a title', content: 'a content' }],
+        selectedNote: { title: 'a title', content: 'a content' },
+      };
+
+      matchSnapshotWithProvider(state);
+    });
   });
 
-  it('should render a prefilled form with selected note', () => {
-    const state = {
-      notes: [{ title: 'a title', content: 'a content' }],
-      selectedNote: { title: 'a title', content: 'a content' },
-    };
+  describe('behavior', () => {
+    it('should dispatch save action when tapping on save button', () => {
+      const selectedNote = {
+        title: 'test title',
+        content: 'test content',
+      };
+      const state = {
+        selectedNote,
+      };
+      const store = createStore(() => state);
+      store.dispatch = jest.fn();
+      const wrapper = mount(
+        <Provider store={store}>
+          <NoteEditorWired />
+        </Provider>,
+      );
+      const input = wrapper.find('TextInput').first();
+      input.prop('onChangeText')('New title');
+      const saveButton = wrapper.find('Button').last();
 
-    matchSnapshotWithProvider(state);
+      saveButton.prop('onPress')();
+
+      expect(store.dispatch).toHaveBeenCalledTimes(1);
+      expect(store.dispatch).toHaveBeenCalledWith({
+        type: 'SAVE_NOTE',
+        payload: {
+          title: 'New title',
+          content: 'test content',
+        },
+      });
+    });
   });
 });
